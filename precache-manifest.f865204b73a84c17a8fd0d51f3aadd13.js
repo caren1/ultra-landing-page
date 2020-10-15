e10d1e108e920ff3c4f46fe42e8dc9f979cfa04f
caren1 <czarnocki.w@wp.pl>
2020-10-15 20:23:43 +0200

    Updates

---
+++ precache-manifest.f865204b73a84c17a8fd0d51f3aadd13.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958d71287ce7800c35ba175bceb25cdc",
+    "url": "/ultra-landing-page/index.html"
+  },
+  {
+    "revision": "8a8d2c12813b0af7b271",
+    "url": "/ultra-landing-page/static/js/2.3dd7ee7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ultra-landing-page/static/js/2.3dd7ee7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2de41bd07a71ddb5d83",
+    "url": "/ultra-landing-page/static/js/main.1073089e.chunk.js"
+  },
+  {
+    "revision": "fd32c2c3649517b68311",
+    "url": "/ultra-landing-page/static/js/runtime-main.9b3ac3e7.js"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "/ultra-landing-page/static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/ultra-landing-page/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/ultra-landing-page/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/ultra-landing-page/static/media/svg-3.397c455a.svg"
+  }
+]);
